08cd19c3051964c4d1cf9562175d82678e64a140
Taulant Ibraj <tibraj94@gmail.com>
2020-04-22 14:50:32 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  extend MetaDancing 
   include Dance #allow use of methods in the included module as isntance methods.
   attr_accessor :name
 
