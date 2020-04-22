79797d161b2999759ed8ef4ec250e6b5e01b5f91
Taulant Ibraj <tibraj94@gmail.com>
2020-04-22 14:47:23 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,11 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid 
-  include Dance 
-  attr_accessor :name 
+class Kid
+  include Dance
+  attr_accessor :name
 
-  def initialize(name) 
-    @name = name 
-  end 
+  def initialize(name)
+    @name = name
+  end
 end 
