c182e0057f1d099b258998347754ab389b7928df
Taulant Ibraj <tibraj94@gmail.com>
2020-04-22 14:52:21 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -8,10 +8,10 @@ module Dance
   end
 
   def pirouette
-    "Im doing a pirouette"
+    "I'm doing a pirouette"
   end
 
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-end 
+end
